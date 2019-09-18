599e467ac8e65c2255d304575fe61d4bbed6e030
Christian Gnitka <webmaster@gnitka.eu>
2019-09-18 14:27:10 +0200

    Fehlerbehandlung in der Main

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -5,16 +5,19 @@ public class Main {
     public static void main(String[] args) {
 
         try {
+
             MyException me = new MyException();
             me.exMeth();
-        } catch (NumberFormatException e) {
-            System.out.println("40 zur basis 4 nicht möglich ...\n" + e.getMessage());
-            System.out.println(e);
-        } catch (ArithmeticException e) {
-            System.out.println("Division durch Null...\n" + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("algemeiner Fehler");
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+            MyException m1 = new MyException();
+
+        } finally {
+            // wird immer ausgeführt selbst wenn es nicht durchläuft
+            System.out.println("Ende finally");
         }
+
+        System.out.println("Ende");
 
 
         /*  int i;
