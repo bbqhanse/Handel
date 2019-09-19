80554c142fb30c3571cddca74ad6574421b69de5
Christian Gnitka <webmaster@gnitka.eu>
2019-09-19 14:58:08 +0200

    Eingabe

--- src/Handel/Datei.java
+++ src/Handel/Datei.java
@@ -1,4 +1,104 @@
 package Handel;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Datei {
+    private int knr;
+    private String vornahme;
+    private String nachnahme;
+    private String plz;
+    private String ort;
+    private String strasse;
+    private String hausnr;
+    private String dateipfad;
+
+    public Datei(String dateipfad) {
+        this.dateipfad = dateipfad;
+    }
+
+    public void schreiben() {
+
+        String kd = knr+"|"+vornahme+"|"+nachnahme+"|"+plz+"|"+ort+"|"+strasse+"|"+hausnr;
+        System.out.println(kd);
+        PrintWriter pWriter = null;
+        try {
+            pWriter =
+                    new PrintWriter(new BufferedWriter(new FileWriter(this.dateipfad, true)));
+            pWriter.println(kd);//"12|Hans|Meier|10245|Berlin|Hausburgstr.|24"
+
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            if (pWriter != null) {
+                pWriter.flush();
+                pWriter.close();
+            }
+        }
+
+    }
+
+    public String lesen() {
+        return "a";
+    }
+
+
+    //getter und setter
+    public int getKnr() {
+        return knr;
+    }
+
+    public void setKnr(int knr) {
+        this.knr = knr;
+    }
+
+    public String getVornahme() {
+        return vornahme;
+    }
+
+    public void setVornahme(String vornahme) {
+        this.vornahme = vornahme;
+    }
+
+    public String getNachnahme() {
+        return nachnahme;
+    }
+
+    public void setNachnahme(String nachnahme) {
+        this.nachnahme = nachnahme;
+    }
+
+    public String getPlz() {
+        return plz;
+    }
+
+    public void setPlz(String plz) {
+        this.plz = plz;
+    }
+
+    public String getOrt() {
+        return ort;
+    }
+
+    public void setOrt(String ort) {
+        this.ort = ort;
+    }
+
+    public String getStrasse() {
+        return strasse;
+    }
+
+    public void setStrasse(String strasse) {
+        this.strasse = strasse;
+    }
+
+    public String getHausnr() {
+        return hausnr;
+    }
+
+    public void setHausnr(String hausnr) {
+        this.hausnr = hausnr;
+    }
 }
--- src/Handel/Main.java
+++ src/Handel/Main.java
@@ -1,11 +1,43 @@
 package Handel;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // todo Neukunden aufnahme
+        int knr;
+        String vorname;
+        String nachnahme;
+        String plz;
+        String ort;
+        String strasse;
+        String hausnr;
+        System.out.println("Kundennummer: ");
+        Scanner sc = new Scanner(System.in);
+        knr = sc.nextInt();
+        System.out.println("Vorname: ");
+        vorname = sc.next();
+        System.out.println("nachname: ");
+        nachnahme = sc.next();
+        System.out.println("Plz: ");
+        plz = sc.next();
+        System.out.println("ort: ");
+        ort = sc.next();
+        System.out.println("Straße: ");
+        strasse = sc.next();
+        System.out.println("hausnr: ");
+        hausnr = sc.next();
 
-
+        Datei d = new Datei("kunde.txt");
+        d.setKnr(knr);
+        d.setVornahme(vorname);
+        d.setNachnahme(nachnahme);
+        d.setPlz(plz);
+        d.setOrt(ort);
+        d.setStrasse(strasse);
+        d.setHausnr(hausnr);
+        d.schreiben();
 
     }
 }
